587551f5d4efaabb22ce374bbe4c9055c92bba2e
Delane Chen <delane4.0@gmail.com>
2019-05-30 11:56:12 -0400

    I have created the majority ofthe logic so far in class. There is no text in the body I am using only alerts and console logs to test information. I currently am able to hit the lose state however the win state is compramised.

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,3 +1,45 @@
-for (i=0; i < 8; i++){
-    document.write("Howdy");
+var wins,losses = 0;
+var attempt = 10;
+var wrongChoice = [];
+var alphabet = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"]
+var cpuLetter = alphabet[Math.floor(Math.random() * alphabet.length)];
+var playerLetter;
+
+alert(cpuLetter);
+
+document.onkeyup = function(event){
+    if(validInput(event.key)){
+        console.log(event.key);
+        if(event.key == cpuLetter){
+            wins ++;
+            tries = 10;
+            wrongChoice = [];
+            var cpuLetter = alphabet[Math.floor(Math.random() * alphabet.length)];
+            alert("you win!");
+        }
+        else{
+            attempt --;
+            wrongChoice.push(event.key);
+            alert(attempt);
+        }
+        if(attempt == 0){
+            losses ++;
+            tries = 10;
+            wrongChoice = [];
+            var cpuLetter = alphabet[Math.floor(Math.random() * alphabet.length)];
+            alert("you lose!");
+        }
+    }
+}
+
+
+
+// this checks if the input is a letter
+function validInput(x){
+    for(i=0;i<alphabet.length;i++){
+        y = alphabet[i];
+        if(x == y){
+            return true;
+        }
+    }
 }
