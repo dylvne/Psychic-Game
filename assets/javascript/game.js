b3b34f96946bf1affa413aec25bc47afc484116f
Delane Chen <delane4.0@gmail.com>
2019-05-30 13:16:01 -0400

    I added in the front end of the page. I checked and made the logic display it's information correctly

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,12 +1,18 @@
-var wins,losses = 0;
+var wins = 0;
+var losses = 0;
 var attempt = 10;
 var wrongChoice = [];
 var alphabet = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"]
 var cpuLetter = alphabet[Math.floor(Math.random() * alphabet.length)];
 var playerLetter;
 
-alert(cpuLetter);
+document.getElementById("wins").innerHTML = "wins: " + wins;
+document.getElementById("losses").innerHTML = "losses: " + losses;
+document.getElementById("attempt").innerHTML = "attempts left: " + attempt;
+document.getElementById("wrongChoice").innerHTML = "Wrong Letters: " + wrongChoice;
 
+
+console.log("cpu Letter: " +cpuLetter);
 document.onkeyup = function(event){
     if(validInput(event.key)){
         console.log(event.key);
@@ -15,21 +21,30 @@ document.onkeyup = function(event){
             attempt = 10;
             wrongChoice = [];
             cpuLetter = alphabet[Math.floor(Math.random() * alphabet.length)];
-            alert("you win!");
+            console.log("cpu Letter: " +cpuLetter);
         }
         else{
             attempt --;
             wrongChoice.push(event.key);
-            alert(attempt);
+            
         }
         if(attempt == 0){
             losses ++;
             attempt = 10;
             wrongChoice = [];
             cpuLetter = alphabet[Math.floor(Math.random() * alphabet.length)];
-            alert("you lose!");
+            console.log("cpu Letter: " +cpuLetter);
         }
     }
+
+    document.getElementById("wins").innerHTML = "wins: " + wins;
+    document.getElementById("losses").innerHTML = "losses: " + losses;
+    document.getElementById("attempt").innerHTML = "attempts left: " + attempt;
+    document.getElementById("wrongChoice").innerHTML = "Wrong Letters: " + wrongChoice;
+
+
+    
+    
 }
 
 
